4218773a00c6d22d1d1486bf4c50dc22b6f22f94
Khanif Nursafi <kkhaniffff@gmail.com>
2025-12-26 13:45:40 +0500

    remove catppuccin color palette, use custom minimal colors instead

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,30 +13,17 @@ if status is-interactive
     end
 
     # Theme
-    set -g fish_color_normal c6d0f5
-    set -g fish_color_command 8caaee
-    set -g fish_color_param eebebe
-    set -g fish_color_keyword ca9ee6
-    set -g fish_color_quote a6d189
-    set -g fish_color_redirection f4b8e4
-    set -g fish_color_end ef9f76
-    set -g fish_color_comment 838ba7
-    set -g fish_color_error e78284
-    set -g fish_color_gray 737994
-    set -g fish_color_selection --background=414559
-    set -g fish_color_search_match --background=414559
-    set -g fish_color_option a6d189
-    set -g fish_color_operator f4b8e4
-    set -g fish_color_escape ea999c
-    set -g fish_color_autosuggestion 737994
-    set -g fish_color_cancel e78284
-    set -g fish_color_cwd e5c890
-    set -g fish_color_user 81c8be
-    set -g fish_color_host 8caaee
-    set -g fish_color_host_remote a6d189
-    set -g fish_color_status e78284
-    set -g fish_pager_color_progress 737994
-    set -g fish_pager_color_prefix f4b8e4
-    set -g fish_pager_color_completion c6d0f5
-    set -g fish_pager_color_description 737994
+    set -g fish_color_normal DADADA
+    set -g fish_color_command 5C9A8E
+    set -g fish_color_param B0B0B0
+    set -g fish_color_keyword 5C9A8E
+    set -g fish_color_quote 9ECE6A
+    set -g fish_color_redirection E0AF68
+    set -g fish_color_end B0B0B0
+    set -g fish_color_error E46876
+    set -g fish_color_comment B0B0B0
+    set -g fish_color_autosuggestion B0B0B0
+    set -g fish_pager_color_progress B0B0B0
+    set -g fish_pager_color_background --background=1E1E1E
+    set -g fish_pager_color_secondary_background --background=262626
 end
